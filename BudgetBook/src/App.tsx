3e79f49b48fab85306eddbc34e6c48d88af5bff4
komekoneko <kai.s20050701@gmail.com>
2026-07-09 21:05:37 +0900

    add input form

--- BudgetBook/src/App.tsx
+++ BudgetBook/src/App.tsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+type Expense = {
+  id: number;
+  name: string;
+  price: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
   return (
     <>
       <h1>BudgetBook</h1>
+      <input
+        type="text"
+        placeholder="項目名"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="金額"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
