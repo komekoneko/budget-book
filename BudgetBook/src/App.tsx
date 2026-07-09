52162ecdc64273d2707829ea2f99d0995112bba5
komekoneko <kai.s20050701@gmail.com>
2026-07-09 21:51:38 +0900

    add 追加機能と合計金額視覚化

--- BudgetBook/src/App.tsx
+++ BudgetBook/src/App.tsx
@@ -11,6 +11,30 @@ function App() {
   const [expenses, setExpenses] = useState<Expense[]>([]);
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
+
+  const addExpense = () => {
+    if(name === "" && price === ""){
+      return alert("項目名と金額を記入してください");
+    }else if(price === ""){
+      return alert("金額を記入してください");
+    }else if(name === ""){
+      return alert("項目名を記入してください");
+    }
+    const newExpense = {
+      id: Date.now(),
+      name: name,
+      price: Number(price),
+    };
+
+    setExpenses([...expenses, newExpense]);
+    setName("");
+    setPrice("");
+  };
+  
+  const total = expenses.reduce((sum, expense) => {
+    return sum + expense.price
+  }, 0)
+
   return (
     <>
       <h1>BudgetBook</h1>
@@ -26,6 +50,13 @@ function App() {
         value={price}
         onChange={(e) => setPrice(e.target.value)}
       />
+      <button onClick={addExpense}>追加</button>
+      <ul>
+        {expenses.map((expense) => {
+          return <li key={expense.id}>{expense.name} : {expense.price}</li>
+        })}
+      </ul>
+      <h2>合計金額: {total}円</h2>
     </>
   );
 }
